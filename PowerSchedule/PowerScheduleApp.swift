02ef5fb4317efa328ffb700b9f1ac57bf5fcae6f
hexiaotian <hexiaotian@sf-express.com>
2023-05-08 16:10:28 +0800

    新建项目

---
+++ PowerSchedule/PowerScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerScheduleApp.swift
+//  PowerSchedule
+//
+//  Created by HeXiaoTian on 2023/5/8.
+//
+
+import SwiftUI
+
+@main
+struct PowerScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
